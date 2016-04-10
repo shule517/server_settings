e65647f9d5ea2e43378780f891142d14ac8cc5a2
shule517 <shule517@gmail.com>
2016-04-10 20:52:40 +0900

    コメントの追加

--- docker_install.sh
+++ docker_install.sh
@@ -1,6 +1,8 @@
+# dockerのインストール
 sudo rpm --import http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6
 sudo yum -y install http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 sudo yum -y install docker-io
 
+# dockerのサービス設定
 sudo service docker start
 sudo chkconfig docker on
